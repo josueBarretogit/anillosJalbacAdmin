b9416c015b9ac61460a96f41f661a9d3d76a021b
josueBarretogit <josuebarretogit@gmail.com>
2023-11-19 21:19:34 -0500

    reduccion redundancia en validationschema

--- src/customHooks/validationSchema/validationSchemaCrearEditar.ts
+++ src/customHooks/validationSchema/validationSchemaCrearEditar.ts
@@ -1,44 +1,57 @@
 import { toTypedSchema } from "@vee-validate/zod";
 import { z } from "zod";
 
-const validationSchemaEditarNombre = {
-  nombre(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-  pesoOro(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-  pesoPlata(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-  categoria(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-  talla(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-  referencia(value: string) {
-    if (!value) {
-      return "Este campo es obligatorio";
-    }
-    return true;
-  },
-};
+const ObjectValidationEditarNombre = z.object({
+  nombre: z
+    .string({ required_error: "Este campo es requerido" })
+    .regex(/[a-zA-Z0-9]$/, "No puedes ingresar caracteres especiales")
+    .nonempty({ message: "Este campo es requerido" }),
+  pesoOro: z.coerce
+    .number({
+      required_error: "Este campo es requerido",
+      invalid_type_error: "Este campo solo admite numeros",
+    })
+    .positive({ message: "No pongas numeros negativos" }),
+  pesoPlata: z.coerce
+    .number({
+      required_error: "Este campo es requerido",
+      invalid_type_error: "Este campo solo admite numeros",
+    })
+    .positive({ message: "No pongas numeros negativos" }),
+  categoria: z.string({ required_error: "Este campo es requerido" }).nonempty(),
+  talla: z.coerce
+    .string({
+      required_error: "Este campo es requerido",
+      invalid_type_error: "Este campo solo admite numeros",
+    })
+    .regex(
+      new RegExp(/^\d+$|([0-9] \/ [0-9])$/),
+      "Solo es valido este formato: numero ó numero / numero",
+    ),
+  referencia: z.coerce
+    .number({
+      required_error: "Este campo es requerido",
+      invalid_type_error: "Este campo solo admite numeros",
+    })
+    .int({ message: "El numero tiene que ser entero" })
+    .positive({ message: "El numero tiene que ser entero" }),
+});
+
+const ObjectValidationCrearNombre = ObjectValidationEditarNombre.extend({
+  imagen: z
+    .any({ required_error: "Este campo es requerido" })
+    .refine((files: File) => files != undefined, "Una imagen es obligatoria")
+    .refine(
+      (file) => file?.[0]?.type.includes("image/"),
+      "Solo se aceptan imagenes",
+    ),
+});
+
+const validationSchemaCrearNombre = toTypedSchema(ObjectValidationCrearNombre);
+
+const validationSchemaEditarNombre = toTypedSchema(
+  ObjectValidationEditarNombre,
+);
 
 const validationSchemaEditarSolitario = {
   formaPiedra(value: string) {
@@ -117,56 +130,6 @@ const validationSchemaEditarDije = {
     return true;
   },
 };
-
-const validationSchemaCrearNombre = toTypedSchema(
-  z.object({
-    nombre: z
-      .string({ required_error: "Este campo es requerido" })
-      .regex(/[a-zA-Z0-9]$/, "No puedes ingresar caracteres especiales")
-      .nonempty({ message: "Este campo es requerido" }),
-    pesoOro: z.coerce
-      .number({
-        required_error: "Este campo es requerido",
-        invalid_type_error: "Este campo solo admite numeros",
-      })
-      .positive({ message: "No pongas numeros negativos" }),
-    pesoPlata: z.coerce
-      .number({
-        required_error: "Este campo es requerido",
-        invalid_type_error: "Este campo solo admite numeros",
-      })
-      .positive({ message: "No pongas numeros negativos" }),
-
-    categoria: z
-      .string({ required_error: "Este campo es requerido" })
-      .nonempty(),
-    talla: z.coerce
-      .string({
-        required_error: "Este campo es requerido",
-        invalid_type_error: "Este campo solo admite numeros",
-      })
-      .regex(
-        new RegExp(/[0-9]$|[0-9] \/ [0-9]$/),
-        "Solo es valido este formato: numero ó numero / numero",
-      ),
-    referencia: z.coerce
-      .number({
-        required_error: "Este campo es requerido",
-        invalid_type_error: "Este campo solo admite numeros",
-      })
-      .int({ message: "El numero tiene que ser entero" }),
-    imagen: z
-      .any({ required_error: "Este campo es requerido" })
-      .refine(
-        (files: File[]) => files?.length == 0,
-        "Una imagen es obligatoria",
-      )
-      .refine(
-        (file) => file?.[0]?.type.includes("image/"),
-        "Solo se aceptan imagenes",
-      ),
-  }),
-);
 
 const validationSchemaCrearSolitario = {
   formaPiedra(value: string) {
